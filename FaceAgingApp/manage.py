34f4adeceddae113a3c08e9c4be9b03e60dc154a
Hanna Konarzewska <hanna.konarzewska00@gmail.com>
2023-01-10 01:14:24 +0100

    Engine Implementation

--- FaceAgingApp/manage.py
+++ FaceAgingApp/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FaceAgingApp.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'face_aging_app.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
