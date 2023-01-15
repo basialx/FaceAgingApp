13d1b870029c98e8ecfe3b50f1667cfc3c8e3731
basialx <91841407+basialx@users.noreply.github.com>
2023-01-15 16:36:46 +0100

    Delete FaceAgingApp directory

--- FaceAgingApp/Image/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Image(models.Model):
-    caption=models.CharField(max_length=100)
-    image=models.ImageField(null=True, blank=True)
-    detected_img = models.ImageField(blank=True)
-    def __str__(self):
-        return self.caption
