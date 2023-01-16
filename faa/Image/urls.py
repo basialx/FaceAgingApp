dfc5c3eb0622726c691507e4b567c290013025c3
basialx <91841407+basialx@users.noreply.github.com>
2023-01-16 18:09:58 +0100

    Add files via upload

--- faa/Image/urls.py
+++ faa/Image/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name="home"),
     path('images/', views.images, name='images'),
     path('<int:id>', views.delete_image, name='delete'),
+    #path('<>', views.redirect_to_index, name='redirect'),
     #path('result/', views.result, name='result'),
 ]
