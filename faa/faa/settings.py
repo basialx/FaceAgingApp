e92313fd0bfc84385229de86694e175e3257fa92
basialx <91841407+basialx@users.noreply.github.com>
2023-01-18 00:46:45 +0100

    Add files via upload

--- faa/faa/settings.py
+++ faa/faa/settings.py
@@ -236,7 +236,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -246,3 +248,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+GDAL_LIBRARY_PATH = r'C:\OSGeo4W64\bin\gdal300'
