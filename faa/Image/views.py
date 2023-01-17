1de4dd4b55a941040eba17f2889ca7bea8cf49c9
Hanna Konarzewska <42982379+Haniaa01@users.noreply.github.com>
2023-01-17 16:01:29 +0100

    Update views.py

--- faa/Image/views.py
+++ faa/Image/views.py
@@ -2,6 +2,10 @@ import glob
 import os
 
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 from .form import ImageForm
 from .models import Image
@@ -39,3 +43,38 @@ def delete_image(request, id):
 
 def undo(request):
     return redirect('index')
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, 'Konto zostało stworzone jako ' + username)
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'register.html', context)
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, ' Niepoprawna nazwa użytkownika lub hasło')
+
+    context = {}
+    return render(request, 'login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
